73bda1c7a84321d22269fa3737509b38be67a667
LeeJongbokZ <s2feeling@naver.com>
2024-11-07 00:17:18 +0900

    first commit

---
+++ src/main/java/com/example/concertTicket_websocket/ConcertTicketWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.concertTicket_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertTicketWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertTicketWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/concertTicket_websocket/ConcertTicketWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concertTicket_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertTicketWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

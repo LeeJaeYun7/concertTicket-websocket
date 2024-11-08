53012905eab28dd290e6eac51fcc88fbc3769d35
LeeJongbokZ <s2feeling@naver.com>
2024-11-08 21:07:40 +0900

    Websocket 기본 설정

--- src/main/java/com/example/concertTicket_websocket/ConcertTicketWebsocketApplication.java
+++ src/main/java/com/example/concertTicket_websocket/ConcertTicketWebsocketApplication.java
@@ -2,8 +2,9 @@ package com.example.concertTicket_websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ConcertTicketWebsocketApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/concertTicket_websocket/greeting/domain/Greeting.java
@@ -0,0 +1,18 @@
+package com.example.concertTicket_websocket.greeting.domain;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
